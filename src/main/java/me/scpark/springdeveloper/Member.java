b61098dd0d787a59f3521adb6d27e0e1895415a5
myspark02 <myspark02@gmail.com>
2026-02-17 22:23:45 +0900

    ORM 실습 코드

--- src/main/java/me/scpark/springdeveloper/Member.java
+++ src/main/java/me/scpark/springdeveloper/Member.java
@@ -15,6 +15,11 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false)
     private Long id;
+
     @Column(name = "name", nullable = false)
     private String name;
+
+    public void changeName(String name) {
+        this.name = name;
+    }
 }
