affd2b93df04efbfb66846f32d868a4989f4d10a
myspark02 <myspark02@gmail.com>
2026-02-22 02:35:21 +0900

    블로그 화면 구성하기

--- src/main/java/me/scpark/springdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/scpark/springdeveloper/SpringBootDeveloperApplication.java
@@ -1,10 +1,13 @@
 package me.scpark.springdeveloper;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
-            SpringApplication.run(SpringBootDeveloperApplication.class, args);
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
 }
